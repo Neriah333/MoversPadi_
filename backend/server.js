0e7ec6dc577718774a9460f5597c5fddaaf1d241
Neriah333 <pheobenyawanda@gmail.com>
2026-02-23 23:46:02 +0300

    Finished the signup - OTP Verification remaining

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+
+
 const app = express();
-const port = 5000;
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// routes
+app.use('/api/auth', require('./routes/authRoutes'));
 
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
